4af4a6e55258c640b8a5f5520bb3c9784740fd10
Christian Magro Pérez <cmagrop@gmail.com>
2026-02-06 11:58:14 -0300

    Se modifica el main.js con el titulo del sistema y se importa bootstrap

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
 import './styles/main.scss';
 import { FormularioReserva } from './FormularioReserva';
 import { ListaReservas } from './ListaReservas';
+
+document.querySelector('#app').innerHTML=`
+<div class="container py-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-10">
+
+            <h1 class="text-center mb-5 text-dark fw-bold">
+                Gestión de Reservas
+            </h1>
+            <div id="form-container">
+            </div>
+            <div id="lista-container" class="mt-5"></div>
+
+        </div>
+
+
+    </div>
+
+
+</div>
+
+
+
+`
