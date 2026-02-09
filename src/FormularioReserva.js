781fcf1ea2a819e44f0290f6ddab2d6df8c44632
Christian Magro Pérez <cmagrop@gmail.com>
2026-02-09 13:01:51 -0300

    se avanzó en FormularioReserva.js , se adiciona soporte html  de torpedo.html

--- src/FormularioReserva.js
+++ src/FormularioReserva.js
@@ -4,9 +4,55 @@ export class FormularioReserva extends Componente{
 
     despuesDeRender(){
 
-        
+        const boton = this.contenedor.querySelector('#btn-agendar');
+            if(boton) //si el boton existe
+            {
+                boton.onclick=
+                ()=>{
+                    const paciente = 
+                    this.contenedor.querySelector('#paciente').value;
+                    const fecha =
+                    this.contenedor.querySelector('#fecha').value;
+                    const hora =
+                    this.contenedor.querySelector('#hora').value;
+
+                    //validar que paciente, fecha y hora no esten vacios
+                    if (paciente && fecha && hora) {
+
+                        this.estado.onAgregar({paciente,fecha,hora});
+                        //limpiar el formulario
+                    
+                    this.contenedor.querySelector('#paciente').value = '';
+                    
+                    this.contenedor.querySelector('#fecha').value = '';
+                    
+                    this.contenedor.querySelector('#hora').value ='';
+                        
+
+                        
+                    } else {
+                        alert("Por favor completa todos los campos");
+                        
+                    }
+                };
+
+
+            }
+
 
 
     }
 
-}
+    html()
+    {
+
+        return 
+        `
+        
+        
+        
+        `;
+    }
+    
+
+} //cierra de la clase
